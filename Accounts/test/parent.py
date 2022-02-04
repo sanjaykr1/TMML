35cea20f0b3bfd8f56d60ad306cafd234abe0ad0
sanjaykr1 <sanjaykr19109@gmail.com>
2022-02-04 10:09:36 +0100

    Parent test class file for common code

---
+++ Accounts/test/parent.py
@@ -0,0 +1,36 @@
+import unittest
+import logging
+from pandas.testing import assert_frame_equal
+from pyspark.sql import SparkSession
+
+
+class PySparkTest(unittest.TestCase):
+
+    @classmethod
+    def create_testing_pyspark_session(cls):
+        return (SparkSession.builder
+                .master('local')
+                .appName('testing')
+                .getOrCreate())
+
+    @classmethod
+    def logger_initializer(cls):
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.logger_initializer()
+        cls.spark = cls.create_testing_pyspark_session()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
+
+    @classmethod
+    def dataframe_equal(cls, df1, df2, columns):
+        df1 = df1.toPandas()
+        df1 = df1.sort_values(by=columns).reset_index(drop=True)
+        df2 = df2.toPandas()
+        df2 = df2.sort_values(by=columns).reset_index(drop=True)
+        assert_frame_equal(df1, df2)
